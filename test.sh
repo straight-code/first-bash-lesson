d4634af8ee03f854d4c3fbf41af29712397a1273
straight-code <97550737+straight-code@users.noreply.github.com>
2022-04-13 22:31:15 +0100

    added screencast

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Enter username:"
+read user
+sleep 1
+echo "Welcome, $user to our first bash lesson!"
+sleep 1
+echo "lets learn bash together"
+sleep 1
+echo "this was made by straight-code"
+sleep 1
+echo "whats your age?"
+read age
+sleep 1
+echo "are you in education?"
+read education
+sleep 1
+
+echo "whats your favourite color?"
+read color
+sleep 1
+
+echo ""
+echo ""
+sleep 1
+
+echo "$user, favourite color is '$color'. $name is $age!"
+
+sleep 1
+
+echo "Eduaction stat:  $education"
